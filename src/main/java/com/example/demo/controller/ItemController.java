b14954db675befd461394d8f95cc66182b3e5883
Takumi Endo <tq-ed@v7.com>
2019-01-12 12:28:59 +0900

    fix: missing RequestMapping

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -5,13 +5,15 @@ import com.example.demo.infra.item.ItemRepository;
 import com.example.demo.infra.item.ItemRepositoryJooq;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-@RestController("api/items")
+@RestController
+@RequestMapping("api/items")
 @RequiredArgsConstructor
 public class ItemController {
 
