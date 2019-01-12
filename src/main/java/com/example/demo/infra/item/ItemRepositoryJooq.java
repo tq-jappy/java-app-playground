e5cf9e92b243ffa557e30781439f7c91df954cfd
Takumi Endo <tq-ed@v7.com>
2019-01-12 18:17:46 +0900

    add insert method

--- src/main/java/com/example/demo/infra/item/ItemRepositoryJooq.java
+++ src/main/java/com/example/demo/infra/item/ItemRepositoryJooq.java
@@ -1,5 +1,6 @@
 package com.example.demo.infra.item;
 
+import com.example.demo.jooq.tables.records.ItemsRecord;
 import lombok.RequiredArgsConstructor;
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
@@ -26,5 +27,11 @@ public class ItemRepositoryJooq {
         return create.selectFrom(ITEMS)
                 .limit(3)
                 .fetchInto(Item.class);
+    }
+
+    public int create(Item item) {
+        ItemsRecord record = create.newRecord(ITEMS, item);
+
+        return create.executeInsert(record);
     }
 }
